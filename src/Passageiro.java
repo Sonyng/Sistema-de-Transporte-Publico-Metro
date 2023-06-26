4117bc190711e620412dcbda7d5e236dc98fc49c
Vinicius Guimarães <110570340+Sonyng@users.noreply.github.com>
2023-06-26 01:50:21 -0300

    Create Passageiro.java

---
+++ src/Passageiro.java
@@ -0,0 +1,11 @@
+public class Passageiro {
+    private String nome;
+
+    public Passageiro(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+}
