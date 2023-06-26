0213907a443424c554e9b964a1ea07be55a0cff6
Vinicius Guimarães <110570340+Sonyng@users.noreply.github.com>
2023-06-26 01:50:51 -0300

    Create Estacao.java

---
+++ src/Estacao.java
@@ -0,0 +1,11 @@
+public class Estacao {
+    private String nome;
+
+    public Estacao(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+}
