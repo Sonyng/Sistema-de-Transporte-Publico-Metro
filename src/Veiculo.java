6485d26b1dff1adfaf8710c11690ecc706b3d6eb
Vinicius Guimarães <110570340+Sonyng@users.noreply.github.com>
2023-06-26 01:49:22 -0300

    Create Veiculo.java

---
+++ src/Veiculo.java
@@ -0,0 +1,11 @@
+public class Veiculo {
+    private java.util.List<Passageiro> passageiros;
+
+    public Veiculo() { /* compiled code */ }
+
+    public void adicionarPassageiro(Passageiro passageiro) { /* compiled code */ }
+
+    public void removerPassageiro(Passageiro passageiro) { /* compiled code */ }
+
+    public java.util.List<Passageiro> getPassageiros() { /* compiled code */ }
+}
